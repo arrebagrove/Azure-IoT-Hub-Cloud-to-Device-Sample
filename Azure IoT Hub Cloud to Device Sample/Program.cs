b4cd6b53c889f71a94d9b2738032a31480f5bbe1
Dave Glover <dglover@microsoft.com>
2016-08-31 16:25:29 +1000

    Add project files.

---
+++ Azure IoT Hub Cloud to Device Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Azure_IoT_Hub_Cloud_to_Device_Sample
+{
+    class Program
+    {
+
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=IoTCampAU.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=UAIZnY/9pwe6CofW6H38EFKv1qvv31UqtjjmVxLGoqk=";
+        static string command = "red";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            while (command != "END")
+            {
+                Console.WriteLine("Enter command (red, green, blue, yellow, end)");
+                command = Console.ReadLine().ToUpper();
+                SendCloudToDeviceMessageAsync(command).Wait();
+            }
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync(string cmd)
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(cmd));
+            await serviceClient.SendAsync("pisense", commandMessage);
+        }
+    }
+}
+
+
